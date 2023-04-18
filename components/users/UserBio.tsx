70611ea2c602e666313edfd26d01989b5772d550
diegosales <diegofelipesales23@gmail.com>
2023-04-18 19:36:29 -0400

    add follow e unfollow

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,37 +1,46 @@
 import { useMemo } from "react";
+import { BiCalendar } from "react-icons/bi";
 import { format } from "date-fns";
 
+import Button from "../Button";
 import useCurrentUser from "../../hooks/useCurrentUser";
 import useUser from "../../hooks/useUser";
-import Button from "../Button";
-import { BiCalendar } from "react-icons/bi";
 import useEditModal from "../../hooks/useEditModal";
+import useFollow from "../../hooks/useFollow";
 
 interface UserBioProps {
   userId: string;
 }
 
-const UserBio: React.FC<UserBioProps> = ({ userId }) => {
+const UserBio: React.FC<UserBioProps> = ({ userId}) => {
   const { data: currentUser } = useCurrentUser();
   const { data: fetchedUser } = useUser(userId);
 
   const editModal = useEditModal();
+
+  const { isFollowing, toggleFollow } = useFollow(userId);
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
       return null;
     }
 
-    return format(new Date(fetchedUser.createdAt), "MMM yyyy");
-  }, [fetchedUser?.createdAt]);
+    return format(new Date(fetchedUser.createdAt), 'MMMM yyyy');
+  }, [fetchedUser?.createdAt])
 
-  return (
+
+  return ( 
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? (
           <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
-          <Button onClick={() => {}} label="Follow" secondary />
+          <Button
+            onClick={toggleFollow} 
+            secondary={!isFollowing}
+            outline={isFollowing}
+            label={isFollowing ? 'Unfollow' : 'Follow'}
+          />
         )}
       </div>
       <div className="mt-8 px-4">
@@ -39,23 +48,27 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           <p className="text-white text-2xl font-semibold">
             {fetchedUser?.name}
           </p>
-          <p className="text-md text-neutral-500">@{fetchedUser?.username}</p>
+          <p className="text-md text-neutral-500">
+            @{fetchedUser?.username}
+          </p>
         </div>
         <div className="flex flex-col mt-4">
-          <p className="text-white">{fetchedUser?.bio}</p>
-          <div
+          <p className="text-white">
+            {fetchedUser?.bio}
+          </p>
+          <div 
             className="
-                        flex
-                        flex-row
-                        items-center
-                        gap-2
-                        mt-4
-                        text-neutral-500
-
-                    "
-          >
+              flex 
+              flex-row 
+              items-center 
+              gap-2 
+              mt-4 
+              text-neutral-500
+          ">
             <BiCalendar size={24} />
-            <p>Joined {createdAt}</p>
+            <p>
+              Joined {createdAt}
+            </p>
           </div>
         </div>
         <div className="flex flex-row items-center mt-4 gap-6">
@@ -63,8 +76,6 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
             <p className="text-white">{fetchedUser?.followingIds?.length}</p>
             <p className="text-neutral-500">Following</p>
           </div>
-        </div>
-        <div className="flex flex-row items-center mt-4 gap-6">
           <div className="flex flex-row items-center gap-1">
             <p className="text-white">{fetchedUser?.followersCount || 0}</p>
             <p className="text-neutral-500">Followers</p>
@@ -72,7 +83,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         </div>
       </div>
     </div>
-  );
-};
-
+   );
+}
+ 
 export default UserBio;
--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -1,13 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import serverAuth from "../../libs/serverAuth";
 
+import serverAuth from "../../libs/serverAuth";
 import prisma from '../../libs/prismadb'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "PATCH") {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'PATCH') {
     return res.status(405).end();
   }
 
@@ -17,24 +14,23 @@ export default async function handler(
     const { name, username, bio, profileImage, coverImage } = req.body;
 
     if (!name || !username) {
-      throw new Error("Missing Fields");
+      throw new Error('Missing fields');
     }
 
-    const updateUser = await prisma.user.update({
-        where: {
-            id: currentUser.id
-        },
-        data: { 
-            name,
-            username,
-            bio,
-            profileImage,
-            coverImage
-        }
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name,
+        username,
+        bio,
+        profileImage,
+        coverImage
+      }
     });
 
-    return res.status(200).json(updateUser);
-
+    return res.status(200).json(updatedUser);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
